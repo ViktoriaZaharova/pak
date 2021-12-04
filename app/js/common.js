efc08b4bd0e72892153f39285ddc03ce713da09f
Viktoria <zaharova181114@gmail.com>
2021-12-04 06:32:04 +0200

    media

--- app/js/common.js
+++ app/js/common.js
@@ -4,7 +4,33 @@ $('[name="phone"]').mask('+7 (999) 999 - 99 - 99');
 $('.clients-slider').slick({
     slidesToShow: 6,
     nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.png" alt=""></button>',
-    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.png" alt=""></button>'
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.png" alt=""></button>',
+    responsive: [
+        {
+            breakpoint: 1430,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
 
 $('.home-slider').slick({
@@ -16,3 +42,13 @@ $('.home-slider').slick({
     appendDots: '.home-slider-nav',
     appendArrows: '.home-slider-nav',
 });
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+new WOW().init();
